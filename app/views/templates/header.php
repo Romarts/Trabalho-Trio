1ca5f84e0b740dc15a0ee9045d19e64ffd5af5ce
Romarts <gabrielromatarta@gmail.com>
2025-11-27 21:57:10 -0300

    Icone site

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Loja Faculdade</title>
+    <title>Arte e Madeira</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
